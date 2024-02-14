77177958b9d31f798bfeaa3f46a2b4693437a3c1
Zainab Abdussalam-Osimade <74374137+zao12345@users.noreply.github.com>
2024-02-13 20:35:42 -0500

    Update calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -7,3 +7,11 @@ test('string with a single number should result in the number itself', () => {
 test('string with two numbers separated by comma should result in the sum of the numbers', () => {
     expect(calculator.add('4,5')).toBe(9);
   }); 
+
+test('string with three numbers separated by comma should result in the sum of the numbers', () => {
+    expect(calculator.add('2,8,4')).toBe(14);
+  });
+
+test('string with four numbers separated by comma should result in the sum of the numbers', () => {
+    expect(calculator.add('2,0,4,5')).toBe(11);
+  });
