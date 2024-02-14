90fe7e78aa19f77ce96cead0b76469f8bd5f4d52
Zainab Abdussalam-Osimade <74374137+zao12345@users.noreply.github.com>
2024-02-13 20:35:55 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ function add(numbers) {
     return numbers
         .split(',')
         .map(x => parseInt(x))
-        .reduce((a, b) => a + b + 1)
+        .reduce((a, b) => a + b)
 }
 
 exports.add = add;
